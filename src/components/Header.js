b403b169bcc149468609e69c328df31230039baf
redhu <sunny.redhu@nokia.com>
2021-05-26 21:58:30 +0530

    Toggable menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { List } from 'react-bootstrap-icons';
 
 const Header = () => {
+  const [menuItemsClassName, setMenuItemsClassName] = useState('rightMenuItems');
+
+  const toggleMenu = () => {
+    console.log("Toggle")
+    if (menuItemsClassName == 'rightMenuItems') {
+      setMenuItemsClassName('rightMenuItemsExpanded')
+    } else if (menuItemsClassName == 'rightMenuItemsExpanded') {
+      setMenuItemsClassName('rightMenuItems')
+    }
+  }
+
   return(
     <div className='Header'>
       <div className='floatMenuLeft'>
-        <ul className='leftMenuItems menuItemLists'>
-          <a href='#introduction'><li className='menuItem'>RICHA JHA</li></a>
+        <ul className='leftMenuItems'>
+          <a href='#introduction'><li className='menuItem headerName'>RICHA JHA</li></a>
         </ul>
       </div>
       <div className='floatMenuRight'>
-        <ul className='rightMenuItems menuItemLists'>
+        <ul className={menuItemsClassName}>
+          <li className='hamburger' onClick={toggleMenu}><List color="#EE4B28" size={50} /></li>
           <a href='#about'><li className='menuItem'>ABOUT</li></a>
           <a href="#skills"><li className='menuItem'>SKILLS</li></a>
           <a href='#experience'><li className='menuItem'>EXPERIENCE</li></a>
